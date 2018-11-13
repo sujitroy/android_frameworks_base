94b31ca9db24312f97e6a38c74b61666dffd8041
bigrushdog <randall.rushing@gmail.com>
2018-11-13 05:30:20 +0000

    base: Update keyguard when font changes
    removed unused variables
    
    Change-Id: If9305cbdeff2dad8e7a4bbcd073d94e20d9720b0
    Signed-off-by: Sujit Roy <kumarsujitroy@gmail.com>

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -31,6 +31,7 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.database.ContentObserver;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Handler;
 import android.provider.Settings;
@@ -81,6 +82,7 @@ public class BatteryMeterView extends LinearLayout implements
     private boolean mBatteryHidden;
     private int mBatteryStyle = BATTERY_STYLE_PORTRAIT;
 
+    private static final String FONT_FAMILY = "sans-serif-medium";
     private BatteryController mBatteryController;
     private SettingObserver mSettingObserver;
     private int mTextColor;
@@ -274,9 +276,11 @@ public class BatteryMeterView extends LinearLayout implements
     }
 
     private void updatePercentText() {
+        Typeface tf = Typeface.create(FONT_FAMILY, Typeface.NORMAL);
         if (mBatteryPercentView != null) {
             mBatteryPercentView.setText(
                     NumberFormat.getPercentInstance().format(mLevel / 100f));
+            mBatteryPercentView.setTypeface(tf);
         }
     }
 
