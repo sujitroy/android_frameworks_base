0ced2065adf4f72d75a879e6d8e48561f2c0266a
TheScarastic <warabhishek@gmail.com>
2019-01-02 18:21:52 +0530

    Implement per-app thermal control [2/2]
    @jhenrique09: First part are on settings and the third on packages/apps/ThermalControl
    
    Active package code by @warabhishek - Original commit:
    
    services: am: Add intent for active package name [2/2]
    
    Change-Id: Ifc3dcab71a8500307d0157d4b143415dd1d25543
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: Sujit Roy <kumarsujitroy@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -27517,4 +27517,9 @@ public class ActivityManagerService extends IActivityManager.Stub
     public boolean shouldForceLongScreen(String packageName) {
         return mLineageActivityManager.shouldForceLongScreen(packageName);
     }
+     Context getContext() {
+        return mContext;
+    }
+
+
 }
