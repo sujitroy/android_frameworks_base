3817a4c9e7f44b9091dfa8065eb537c9eb0ec126
IacobIonut01 <paulionut2003@gmail.com>
2020-04-26 19:37:06 +0300

    Settings: Battery: Redesign BatteryMeterView
    Port DotOS-P BatteryMeterView to DotOS-Q
    
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Idbaaded83295c205341f1ca3a9c53d92ff26b4ef
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: Sujit Roy <kumarsujitroy@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -50,7 +50,7 @@ import androidx.annotation.VisibleForTesting;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.app.LoaderManager.LoaderCallbacks;
 import androidx.loader.content.Loader;
-
+import androidx.preference.Preference;
 import com.android.settings.R;
 import com.android.settings.SettingsActivity;
 import com.android.settings.Utils;
@@ -178,12 +178,10 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     protected void updateViews(List<BatteryInfo> batteryInfos) {
         final BatteryMeterView batteryView = mBatteryLayoutPref
                 .findViewById(R.id.battery_header_icon);
-        final TextView percentRemaining =
-                mBatteryLayoutPref.findViewById(R.id.battery_percent);
         final TextView summary1 = mBatteryLayoutPref.findViewById(R.id.summary1);
         BatteryInfo oldInfo = batteryInfos.get(0);
         BatteryInfo newInfo = batteryInfos.get(1);
-        percentRemaining.setText(Utils.formatPercentage(oldInfo.batteryLevel));
+        batteryView.setText(Utils.formatPercentage(oldInfo.batteryLevel));
 
         // set the text to the old estimate (copied from battery info). Note that this
         // can sometimes say 0 time remaining because battery stats requires the phone
@@ -268,7 +266,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         }
         return super.onPreferenceTreeClick(preference);
     }
-	
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -471,7 +469,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
 
             batteryView.setBatteryLevel(mBatteryLevel);
             batteryView.setPowerSave(mPowerManager.isPowerSaveMode());
-            timeText.setText(formatBatteryPercentageText(mBatteryLevel));
+            batteryView.setText(formatBatteryPercentageText(mBatteryLevel));
         }
     }
 
@@ -492,7 +490,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                     final Integer level = (Integer) animation.getAnimatedValue();
                     batteryView.setBatteryLevel(level);
                     batteryView.setPowerSave(mPowerManager.isPowerSaveMode());
-                    timeTextView.setText(formatBatteryPercentageText(level));
+                    batteryView.setText(formatBatteryPercentageText(level));
                 }
             });
             animator.start();
